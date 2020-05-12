bb819648909ffcebe194dd22357f6dd7b8b61a2b
Valentin <v.bondzio@gmail.com>
2020-05-12 12:31:45 +0100

    Create Remove-vNETdriver-wrapper.ps1
    output looks like this if not exported to csv:
    
    VM Name            : WinTest2
    VM IP              : 10.27.41.23
    Tools Version      : 10341
    Guest Configured   : windows8Server64Guest
    Guest Detected     : windows8Server64Guest
    Windows Version    : 6.3.9600.0
    PowerShell Version : 4.0
    Execution Policy   : RemoteSigned
    File Copied        : True
    Script Result      : no issue

---
+++ Remove-vNETdriver-wrapper.ps1
@@ -0,0 +1,120 @@
+function Remove-vNETdriver-wrapper {
+    <#
+    .SYNOPSIS
+        Runs RemoveOldVNetDriver.ps1 on all powered on Windows VMs and report back whether they have to be rebooted
+    .DESCRIPTION
+        Gets some basic VM information and tries to invoke the script from https://kb.vmware.com/s/article/78016 to disable the old vNet driver. Reports back which VMs have to be restarted
+    .NOTES
+        Author:     Valentin
+        GitHub:     https://github.com/vbondzio/sowasvonunsupported/blob/master/Remove-vNETdriver-wrapper.ps1
+        Liability:  Absolutely none, I didn't even fully test this for all possible returns.
+    .PARAMETER ClusterName
+        The cluster you want to connect and retreive the topology information for. Runs against all clusters / hosts if not specified. Doesn't support resourcepools (will only get VMs directly under the cluster).
+    .PARAMETER VMname
+        The VM you want to connect and retreive the topology information for. Runs against all clusters / hosts if not specified. Doesn't work together with ClusterName parameter.
+    .PARAMETER Run
+        Will copy and run the removal script, ommiting will just get VM / Windows info
+    .EXAMPLE
+        PS> Remove-vNETdriver-wrapper
+    .EXAMPLE
+        PS> Remove-vNETdriver-wrapper -Run
+    .EXAMPLE
+        PS> Remove-vNETdriver-wrapper -ClusterName "MyCluster" -Run
+    #>
+
+    param(
+        [Parameter(Mandatory = $false)] [string]$ClusterName,
+        [Parameter(Mandatory = $false)] [string]$VMname,
+        [Parameter(Mandatory = $false)] [switch]$Run
+    )
+
+    $guestUser = "Administrator"
+    $guestPassword = "VMware123!"
+    $guestRemoteFolder ="C:\Windows\Temp\"
+    
+    $scriptName = "RemoveOldVNetDriver.ps1"
+    $scriptOriginalMd5sum = "7D3F050F4A0C19182625E0B7F8C1278E"
+    $localFolder ="c:\tmp\"
+    $localScriptToUpload = "$localFolder"+"$scriptName"
+
+    $runDateTime = (Get-Date).ToString('yyyy-MM-dd_HH-mm')
+    $csvExportFilename = "Remove-vNETdriver-wrapper_export@$runDateTime.csv"
+
+    $scriptCurrentMd5sum = $(Get-FileHash $localScriptToUpload -Algorithm MD5).Hash
+
+    if ($scriptCurrentMd5sum -ne $scriptOriginalMd5sum) {
+        Write-Host "The MD5 checksum of: "$localScriptToUpload" ($scriptCurrentMd5sum), doesn't match the one this script was written for ($scriptOriginalMd5sum).`nExiting"
+        Exit
+    }
+
+    if($ClusterName) {
+        $cluster = Get-View -ViewType ClusterComputeResource -Property Name,ResourcePool -Filter @{"Name"="$ClusterName"}
+        $vms = Get-View ((Get-View $cluster.ResourcePool).VM) -Property Name,Summary.Vm,Runtime.PowerState,Config.GuestId,Guest
+    } elseif ($VMname) {
+        $vms = Get-View -ViewType VirtualMachine -Property Name,Summary.Vm,Runtime.PowerState,Config.GuestId,Guest -Filter @{"Name"="$VMname"}
+    } else {
+        $vms = Get-View -ViewType VirtualMachine -Property Name,Summary.Vm,Runtime.PowerState,Config.GuestId,Guest
+    }
+
+    $allResults = @()
+    foreach ($vm in $vms | Where-Object {
+            $_.Runtime.PowerState -eq "poweredOn" -and $_.Config.GuestId -like "windows*"
+        } | Sort-Object -Property Name) {
+        
+        $fullVmObject = Get-VM -Id $vm.Summary.Vm
+        
+        $windowsVersion = "N/A"
+        $psVersion = "N/A"
+        $executionPolicy = "N/A"
+        $fileCopied = "N/A"
+        $scriptResult = "N/A"
+
+        $debugStats = Invoke-VMScript -vm $fullVmObject -ScriptText "[System.Environment]::OSVersion.Version.ToString(); (Get-Host).Version.ToString(); Get-ExecutionPolicy" -GuestUser $guestUser -GuestPassword $guestPassword -ScriptType Powershell
+        
+        if ($debugStats) {
+            $debugStats = $debugStats.Split("`n")
+            $windowsVersion = $debugStats[0]
+            $psVersion = $debugStats[1]
+            $executionPolicy = $debugStats[2]
+        }
+
+        if ($Run) {
+            Copy-VMGuestFile -VM $fullVmObject -LocalToGuest -Source $localScriptToUpload -Destination $guestRemoteFolder -GuestUser $guestUser -GuestPassword $guestPassword
+            $fileCopied = $?
+            
+            if ($?){
+                $scriptOutput = Invoke-VMScript -vm $fullVmObject -ScriptText "powershell.exe -ExecutionPolicy Bypass -File $guestRemoteFolder$scriptName" -GuestUser $guestUser -GuestPassword $guestPassword -ScriptType Powershell
+
+                Switch -Regex ($scriptOutput) { 
+                    '.*you must reboot Windows to stop the driver from running.' {
+                        $scriptResult = "needs reboot"
+                    } 
+                    '.*driver service is stopped and deleted.' {
+                        $scriptResult = "removed"
+                    } 
+                    '.*no issue.' {
+                        $scriptResult = "no issue"
+                    } 
+                    Default {
+                        $scriptResult = $scriptOutput
+                    } 
+                } 
+            }
+        }
+
+        $currentResults = [pscustomobject] @{
+            "VM Name" = $vm.Name
+            "VM IP" = $vm.Guest.IpAddress
+            "Tools Version" = $vm.Guest.ToolsVersion
+            "Guest Configured" = $vm.Config.GuestId
+            "Guest Detected" = $vm.Guest.GuestId
+            "Windows Version" = $windowsVersion
+            "PowerShell Version" = $psVersion
+            "Execution Policy" = $executionPolicy
+            "File Copied" = $fileCopied
+            "Script Result" = $scriptResult
+        }
+        $allResults += $currentResults
+    }
+    $allResults | Export-Csv -Path $localFolder$csvExportFilename -NoTypeInformation
+}
