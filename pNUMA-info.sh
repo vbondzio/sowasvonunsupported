5e83a53f5f391d9a9171e27b7ead63ea0b6c1c62
Valentin <v.bondzio@gmail.com>
2020-08-22 18:22:14 +0100

    Update pNUMA-info.sh

--- pNUMA-info.sh
+++ pNUMA-info.sh
@@ -2,24 +2,19 @@
 # totally not supported
 # questions / bugs? email: vbondzio
 
-htstate=$(vsish -e get /hardware/cpu/cpuInfo | sed -n 's/^   Hyperthreading state:Hyperthreading state: 3 -> \(.*\)$/\1/p');
-packages=$(vsish -e get /hardware/cpu/cpuInfo | sed -n 's/^   Number of packages:\([0-9]\)$/\1/p');
-
-echo -e "Hyperthreading: $htstate\nPackages: "$packages
-
+htstate=$(vsish -e get /hardware/cpu/cpuInfo | sed -n 's/^   Hyperthreading state:\(.*\)$/\1/p')
+packages=$(vsish -e get /hardware/cpu/cpuInfo | sed -n 's/^   Number of packages:\([0-9]\)$/\1/p')
+nodes=$(sched-stats -t ncpus | sed -n 's/\([0-9]\+\) NUMA nodes$/\1/p')
+echo -e "Hyperthreading: $htstate\nPackages: $packages\nNodes: $nodes"
 vsish -e ls /memory/nodeList/ | sort -n | while read node
 do
-    pcpus=$(vsish -e ls /hardware/numa/${node}/pcpus/ | wc -l)
-    
-    if [[ "$htstate" == "enabled" ]]
-    then
-            cores=$(expr $pcpus / 2)
-    else
-            cores=$pcpus
-    fi
-
-    totalpages=$(vsish -e get /memory/nodeList/${node} | sed -n 's/^   Total (pages):\([0-9]\+\)$/\1/p')
-    freepages=$(vsish -e get /memory/nodeList/${node} | sed -n 's/^   Free (pages):\([0-9]\+\)$/\1/p')
-
-    echo -e "\nNUMA Node ${node}" \\n "PCPUs: "$pcpus \\n "Cores: $cores"  \\n "Total Mem: "$(expr ${totalpages} \* 4 / 1024 / 1024)" GB" \\n "Free Mem: "$(expr ${freepages} \*
+        pcpus=$(vsish -e ls /hardware/numa/${node}/pcpus/ | wc -l)
+        if [[ "$htstate" == "enabled" ]]; then
+                cores=$(expr $pcpus / 2)
+        else
+                cores=$pcpus
+        fi
+        totalpages=$(vsish -e get /memory/nodeList/${node} | sed -n 's/^   Total (pages):\([0-9]\+\)$/\1/p')
+        freepages=$(vsish -e get /memory/nodeList/${node} | sed -n 's/^   Free (pages):\([0-9]\+\)$/\1/p')
+        echo -e "\nNUMA Node ${node}" \\n "PCPUs: "$pcpus \\n "Cores: $cores"  \\n "Total Mem: "$(expr ${totalpages} \* 4 / 1024 / 1024)" GB" \\n "Free Mem: "$(expr ${freepages} \* 4 / 1024 / 1024)" GB"
 done
