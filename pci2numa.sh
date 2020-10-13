d6a110f6f694c2ec68a72d2e6de64e4020e2719d
Valentin <v.bondzio@gmail.com>
2020-10-13 17:55:47 +0100

    Create pci2numa.sh
    Quick check for all PCI devices, needs 6.7, pipe to grep "\[vm" to filter for "vmhba, vmnic, etc." devices

---
+++ pci2numa.sh
@@ -0,0 +1,18 @@
+lspci=$(lspci)
+for bus in $(vsish -e ls /hardware/pci/seg/0/bus/)
+do
+  for slot in $(vsish -e ls /hardware/pci/seg/0/bus/${bus}slot/)
+  do
+    for func in $(vsish -e ls /hardware/pci/seg/0/bus/${bus}slot/${slot}func/)
+    do
+      pciConfigHeader=$(vsish -e get /hardware/pci/seg/0/bus/${bus}slot/${slot}func/${func}pciConfigHeader)
+      numaAssignedDevice=$(echo "${pciConfigHeader}" | sed -n 's/   Numa node:\([0-9]\+\)$/\1/p')
+      if [ ${numaAssignedDevice} != "4294967295" ]
+      then
+        xbus=$(printf "%02x\n" ${bus%?})
+        xslot=$(printf "%02x\n" ${slot%?})
+        description=$(echo "${lspci}" | grep "0000:${xbus}:${xslot}.${func%?}" | cut -d " " -f 2-)
+        echo ${numaAssignedDevice} - ${description}
+      fi
+    done
+  done
