e71ef808bfc14d334262e14c58d2e5cfb847e59b
Valentin <v.bondzio@gmail.com>
2021-01-20 17:39:08 +0000

    Update pstates_disabled_check.sh
    added description

--- pstates_disabled_check.sh
+++ pstates_disabled_check.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+# https://github.com/vbondzio/sowasvonunsupported/blob/master/pstates_disabled_check.sh
+# Checks whether the HW is disabling any P-States, preventing ESXi from using them. Exits if ESXi can't control P-States in the first place.
+
 hwSupport=$(vsish -e get /power/hardwareSupport | sed -n 's/   CPU power management:\(ACPI P-states.*\)$/\1/p')
 if [ -z "$hwSupport" ]; then
     echo "No P-States presented to ESXi"; exit 0
