4cc4a2d6870c7d0a601c58d76e2bba681a3cf848
Valentin <v.bondzio@gmail.com>
2021-01-20 16:59:32 +0000

    Create pstates_disabled_check.sh
    Check if / how many PCPUs got which P-States disabled by the HW. Most likely due to thermal / power capping events or LOM bugs.

---
+++ pstates_disabled_check.sh
@@ -0,0 +1,16 @@
+hwSupport=$(vsish -e get /power/hardwareSupport | sed -n 's/   CPU power management:\(ACPI P-states.*\)$/\1/p')
+if [ -z "$hwSupport" ]; then
+    echo "No P-States presented to ESXi"; exit 0
+fi
+
+for pcpu in $(vsish -e ls /power/pcpu/)
+do 
+	for pstate in $(vsish -e ls /power/pcpu/${pcpu}pstate/)
+	do 
+		avail=$(vsish -e get /power/pcpu/${pcpu}pstate/${pstate} | sed -n  's/^   Available: \([0-1]\)$/\1/p')
+		if [ $avail == "0" ]
+		then
+			echo "${pstate%%/}"
+		fi
+	done
+done | sort -n | uniq -c | awk '{ print "P"$2" is disabled on "$1" PCPUs" }'
