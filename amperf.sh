d3684c48ddcc94676190433bf91daed8afb60c1f
Valentin <v.bondzio@gmail.com>
2021-02-07 15:01:34 +0000

    Create amperf.sh

---
+++ amperf.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# https://github.com/vbondzio/sowasvonunsupported/blob/master/amperf.sh
+# Prints avg. of core frequency per package in percent
+
+amperfSum=0
+amperfCount=0
+
+printf "%+8s  %+7s  %+6s\n" "Package" "PCPU" "AMPERF"
+
+for package in $(vsish -e ls /hardware/cpuTopology/package/nodes/)
+do 
+	pcpurange=$(vsish -e get /hardware/cpuTopology/package/nodes/${package} | sed -n 's/   pcpus:\([0-9-]\+\)$/\1/p')
+	pcpus=$(echo ${pcpurange} | sed 's/-/ /g')
+	for pcpu in $(seq ${pcpus})
+	do
+		perf1=$(vsish -e get /power/pcpu/${pcpu}/perf)
+		aperf1=$(echo "$perf1" | sed -n  's/^   APERF reading: \([0-9]\+\)$/\1/p')
+		mperf1=$(echo "$perf1" | sed -n  's/^   MPERF reading: \([0-9]\+\)$/\1/p')
+		
+		perf2=$(vsish -e get /power/pcpu/${pcpu}/perf)
+		aperf2=$(echo "$perf2"	| sed -n  's/^   APERF reading: \([0-9]\+\)$/\1/p')
+		mperf2=$(echo "$perf2" | sed -n  's/^   MPERF reading: \([0-9]\+\)$/\1/p')
+
+		amperfPct=$(printf %.2f\\n "$(( 100 * (${aperf2} - ${aperf1}) / (${mperf2} - ${mperf1}) ))")
+		amperfSum=$(($amperfSum + ${amperfPct%.*}))
+		amperfCount=$((amperfCount+1))
+	done
+	amperfPct=$(( $amperfSum / $amperfCount ))
+	printf "%+8s  %+7s  %+6s\n" "${package}" "${pcpurange}" "${amperfPct%.*}"
+done
