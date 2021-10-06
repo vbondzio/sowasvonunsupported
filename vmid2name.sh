9301aff2c59328d61acd25787f8a6a19c765d182
Valentin <v.bondzio@gmail.com>
2021-10-06 14:02:03 +0100

    Update vmid2name.sh

--- vmid2name.sh
+++ vmid2name.sh
@@ -7,5 +7,5 @@ do
     group=$(vsish -e get /sched/Vcpus/${cartel}/groupID | sed 's/[ \t]*$//')
     vcpulead=$(vsish -e get /sched/groups/${group}/vcpuLeaderID | sed 's/[ \t]*$//')
     vmname=$(vsish -e get /world/${vcpulead}/name | cut -d : -f 2-)
-    echo -e "CID: ${cartel}\tGID: ${group}\tvcpuLeaderID: ${vcpulead}\tName: ${vmname%%/}"
+    echo -e "WID: ${vm%%/}\tCID: ${cartel}\tGID: ${group}\tvcpuLeaderID: ${vcpulead}\tName: ${vmname%%/}"
 done
