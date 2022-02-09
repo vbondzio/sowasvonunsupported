7032ed9c1434eebf3d564f954e573c3578706c96
Valentin <v.bondzio@gmail.com>
2022-02-09 18:43:48 +0000

    include "all" switch
    mostly for pre 6.5 hosts ... I know

--- amperf.sh
+++ amperf.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # https://github.com/vbondzio/sowasvonunsupported/blob/master/amperf.sh
 # Prints avg. of core frequency per package in percent
+# call with "all" to list all PCPUs instead of averaging per package, defaults to per socket
 
 amperfSum=0
 amperfCount=0
@@ -22,9 +23,19 @@ do
 		mperf2=$(echo "$perf2" | sed -n  's/^   MPERF reading: \([0-9]\+\)$/\1/p')
 
 		amperfPct=$(printf %.2f\\n "$(( 100 * (${aperf2} - ${aperf1}) / (${mperf2} - ${mperf1}) ))")
-		amperfSum=$(($amperfSum + ${amperfPct%.*}))
-		amperfCount=$((amperfCount+1))
+		
+		if [ "$1" == "all" ]
+		then
+			printf "%+8s  %+7s  %+6s\n" "${package}" "${pcpu}" "${amperfPct%.*}"
+		else
+			amperfSum=$(($amperfSum + ${amperfPct%.*}))
+			amperfCount=$((amperfCount+1))
+		fi
 	done
-	amperfPct=$(( $amperfSum / $amperfCount ))
-	printf "%+8s  %+7s  %+6s\n" "${package}" "${pcpurange}" "${amperfPct%.*}"
+	
+	if [ "$1" != "all" ]
+	then
+		amperfPct=$(( $amperfSum / $amperfCount ))
+		printf "%+8s  %+7s  %+6s\n" "${package}" "${pcpurange}" "${amperfPct%.*}"
+	fi
 done
