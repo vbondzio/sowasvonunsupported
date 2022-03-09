5d8a8eac97796549d1773f42cf22c1084f225ddc
Valentin <v.bondzio@gmail.com>
2022-03-09 21:47:08 +0000

    limit display name to 24 characters
    I don't want to upset the table  ...

--- vmid2name.sh
+++ vmid2name.sh
@@ -4,15 +4,20 @@
 # we don't want errors by default because some vsi nodes might already be partially destroyed
 exec 2> /dev/null
 
-printf "%+12s %+12s %+12s %+12s    %-42s %-42s\n" "WID" "CID" "GID" "LWID" "displayName" "workDir"
+printf "%+12s %+12s %+12s %+12s    %-25s %-30s\n" "WID" "CID" "GID" "LWID" "displayName" "workDir"
 # https://stackoverflow.com/questions/38861895/how-to-repeat-a-dash-hyphen-in-shell
-printf -- '-%.0s' $(seq 160); echo ""
+printf -- '-%.0s' $(seq 150); echo ""
 for vm in $(vsish -e ls /vm/ | sort -n)
 do
-	cartel=$(vsish -e get /vm/${vm}vmxCartelID)
-	group=$(vsish -e get /sched/Vcpus/${cartel}/groupID | sed 's/[ \t]*$//')
-	vcpulead=$(vsish -e get /sched/groups/${group}/vcpuLeaderID | sed 's/[ \t]*$//')
-	vmname=$(vsish -e get /world/${vcpulead}/name | cut -d : -f 2-)
-	dir=$(vsish -e get /userworld/cartel/${cartel}/cmdline | grep -o /vmfs/volumes.* | cut -d / -f 4-5)
-	printf "%+12s %+12s %+12s %+12s    %-42s %-42s\n" ${vm%%/} ${cartel} ${group} ${vcpulead} "${vmname%%/}" "${dir}"
+        cartel=$(vsish -e get /vm/${vm}vmxCartelID)
+        group=$(vsish -e get /sched/Vcpus/${cartel}/groupID | sed 's/[ \t]*$//')
+        vcpulead=$(vsish -e get /sched/groups/${group}/vcpuLeaderID | sed 's/[ \t]*$//')
+        vmname=$(vsish -e get /world/${vcpulead}/name | cut -d : -f 2-)
+        if [[ "${#vmname}" -ge "24" ]]
+        then
+                vmnameShort=$(echo ${vmname} | cut -c -20)
+                vmname="${vmnameShort}(..)"
+        fi
+        dir=$(vsish -e get /userworld/cartel/${cartel}/cmdline | grep -o /vmfs/volumes.* | cut -d / -f 4-5)
+        printf "%+12s %+12s %+12s %+12s    %-25s %-35s\n" ${vm%%/} ${cartel} ${group} ${vcpulead} "${vmname%%/}" "${dir}"
 done
