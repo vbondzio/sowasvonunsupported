d0ce08aa6e5b5a52569314d04c9aa05661ba5beb
Valentin <v.bondzio@gmail.com>
2023-02-17 10:39:04 +0000

    Update c1e_switch.sh
    no idea why I used the last PCPU in a package, I'm 99% sure there was no reason so changing to make it more intuitive / not to confuse. also fixed whitespaces

--- c1e_switch.sh
+++ c1e_switch.sh
@@ -16,17 +16,17 @@ switchedC1eMsr=$(printf "0x%x\n" "$((${powerControlMsr} ^ ${MSR_POWER_CTL_C1E}))
 checkAllPcpusForPowerControlMsrValue () {
     # the check is really just for debug / sanity checking, the MSR should be synced across cores in a package and checking both threads is probably unnecessary 
 
-	powerControlMsrUnique=$(
-		for allPcpus in $(vsish -e ls /hardware/cpu/cpuList/)
-		do
-			vsish -e get /hardware/msr/pcpu/${allPcpus}/addr/${MSR_POWER_CTL} 
-		done | uniq | wc -l
-	)
-	
-	if [ ${powerControlMsrUnique} != "1" ] 
-	then
-		echo -e "something is really wrong, call an adult!" >&2
-	fi
+    powerControlMsrUnique=$(
+        for allPcpus in $(vsish -e ls /hardware/cpu/cpuList/)
+        do
+            vsish -e get /hardware/msr/pcpu/${allPcpus}/addr/${MSR_POWER_CTL} 
+        done | uniq | wc -l
+    )
+    
+    if [ ${powerControlMsrUnique} != "1" ] 
+    then
+        echo -e "something is really wrong, call an adult!" >&2
+        fi
 }
 
 checkC1eEnabled () {
@@ -48,10 +48,10 @@ $(checkAllPcpusForPowerControlMsrValue)
 numPackages=$(sched-stats -t ncpus | sed -n 's/^[ \t]*\([0-9]\+\) packages$/\1/p')
 numPcpus=$(sched-stats -t ncpus | sed -n 's/^[ \t]*\([0-9]\+\) PCPUs$/\1/p')
 
-for lastPcpuEachPackage in $(seq $((${numPcpus} / ${numPackages} - 1)) $((${numPcpus} / ${numPackages})) ${numPcpus})
+for firstPcpuEachPackage in $(seq 0 $((${numPcpus} / ${numPackages})) $((${numPcpus} - 1)))
 do
-    echo -e "writing ${switchedC1eMsr} to PCPU ${lastPcpuEachPackage} at ${MSR_POWER_CTL}"
-    vsish -e set /hardware/msr/pcpu/${lastPcpuEachPackage}/addr/${MSR_POWER_CTL} ${switchedC1eMsr}
+    echo -e "writing ${switchedC1eMsr} to PCPU ${firstPcpuEachPackage} at ${MSR_POWER_CTL}"
+    vsish -e set /hardware/msr/pcpu/${firstPcpuEachPackage}/addr/${MSR_POWER_CTL} ${switchedC1eMsr}
 done
 
 $(checkAllPcpusForPowerControlMsrValue)
