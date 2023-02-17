384af2fb6e446eca4c17793966c4825ca6766569
Valentin <v.bondzio@gmail.com>
2023-02-17 10:21:11 +0000

    reduced verbosity

--- c1e_switch.sh
+++ c1e_switch.sh
@@ -13,40 +13,46 @@ MSR_POWER_CTL_C1E=0x00000002
 powerControlMsr=$(vsish -e get /hardware/msr/pcpu/0/addr/${MSR_POWER_CTL})
 switchedC1eMsr=$(printf "0x%x\n" "$((${powerControlMsr} ^ ${MSR_POWER_CTL_C1E}))") # XOR
 
-numPackages=$(sched-stats -t ncpus | sed -n 's/^[ \t]*\([0-9]\+\) packages$/\1/p')
-numCores=$(sched-stats -t ncpus | sed -n 's/^[ \t]*\([0-9]\+\) cores$/\1/p')
-numThreads=$(sched-stats -t ncpus | sed -n 's/^[ \t]*\([0-9]\+\) PCPUs$/\1/p')
+checkAllPcpusForPowerControlMsrValue () {
+    # the check is really just for debug / sanity checking, the MSR should be synced across cores in a package and checking both threads is probably unnecessary 
 
-checkAllCoresForPowerControlMsrValue () {
-    # the all core check is really just for debug / sanity checking, the MSR should be synced across cores in a package
-    echo -e "\nChecking the value of MSR_POWER_CTL (${MSR_POWER_CTL}) for all ${numCores} cores:" >&2
-    for powerControlMsrOfCore in $(seq 0 2 $((${numThreads} - 1)))
-    do
-        echo -e "PCPU ${powerControlMsrOfCore} - $(vsish -e get /hardware/msr/pcpu/${powerControlMsrOfCore}/addr/${MSR_POWER_CTL})" >&2
-    done
+	powerControlMsrUnique=$(
+		for allPcpus in $(vsish -e ls /hardware/cpu/cpuList/)
+		do
+			vsish -e get /hardware/msr/pcpu/${allPcpus}/addr/${MSR_POWER_CTL} 
+		done | uniq | wc -l
+	)
+	
+	if [ ${powerControlMsrUnique} != "1" ] 
+	then
+		echo -e "something is really wrong, call an adult!" >&2
+	fi
 }
 
 checkC1eEnabled () {
     # according to the MSR value on PCPU0
     powerControlMsr=$(vsish -e get /hardware/msr/pcpu/0/addr/${MSR_POWER_CTL})
     c1eEnabledMsr=$(((${powerControlMsr} & ${MSR_POWER_CTL_C1E}) !=0)) # AND
-    
+
     if [ "${c1eEnabledMsr}" == "1" ]
     then
-        echo -e "\nC1E enabled" >&2 
+        echo -e "C1E enabled" >&2
     else
-        echo -e "\nC1E disabled" >&2
+        echo -e "C1E disabled" >&2
     fi
 }
 
 $(checkC1eEnabled)
-$(checkAllCoresForPowerControlMsrValue)
+$(checkAllPcpusForPowerControlMsrValue)
 
-for lastPcpuEachPackage in $(seq $((${numThreads} / ${numPackages} - 1)) $((${numThreads} / ${numPackages})) ${numThreads})
+numPackages=$(sched-stats -t ncpus | sed -n 's/^[ \t]*\([0-9]\+\) packages$/\1/p')
+numPcpus=$(sched-stats -t ncpus | sed -n 's/^[ \t]*\([0-9]\+\) PCPUs$/\1/p')
+
+for lastPcpuEachPackage in $(seq $((${numPcpus} / ${numPackages} - 1)) $((${numPcpus} / ${numPackages})) ${numPcpus})
 do
-    echo -e "\nwriting ${switchedC1eMsr} to PCPU ${lastPcpuEachPackage}"
+    echo -e "writing ${switchedC1eMsr} to PCPU ${lastPcpuEachPackage} at ${MSR_POWER_CTL}"
     vsish -e set /hardware/msr/pcpu/${lastPcpuEachPackage}/addr/${MSR_POWER_CTL} ${switchedC1eMsr}
 done
 
-$(checkAllCoresForPowerControlMsrValue)
+$(checkAllPcpusForPowerControlMsrValue)
 $(checkC1eEnabled)
